b768ede8a002e553be33a4876ae9dc0e23b90853
Cristina Rodriguez <yosola@gmail.com>
2023-02-22 12:09:02 -0800

    Router for a book working

--- index.js
+++ index.js
@@ -15,6 +15,18 @@ app.get('/api/books', (req, res) => {
         res.json(books);
 })
 
+app.get('/api/books/:bookID', cors(), async (req, res) =>{
+    //Is the request
+    let requestedBook = req.params.bookID;
+    console.log(requestedBook);
+    //Is the response
+    for(let book of books){
+        if(book.isbn === requestedBook){
+            res.json(book);
+        }
+    }
+})
+
 const PORT = 5000;
 
 app.listen(PORT, () => console.log(`Hola! Server is running on port ${PORT}`));
